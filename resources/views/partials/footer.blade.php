0e0140366d85fd94faf36de9a81a50864913ddff
MUSHLIHUN <mushlihun@asiapay.com>
2023-10-06 17:25:37 +0700

    update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="text-center my-3">
-  <span class="text-secondary">Created by <a class="text-secondary" href="https://www.github.com/wisuja">wisuja</a> &copy; 2021</span>
+  <span class="text-secondary">Created by <a class="text-secondary" href="https://www.github.com/mushlihun">Lihun</a> &copy; 2023</span>
 </footer>
